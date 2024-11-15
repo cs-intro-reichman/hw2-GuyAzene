3932059b16af562cc34268f9bd81860f7e7b204d
Guy Azene <azene.guy@gmail.com>
2024-11-15 13:36:29 +0200

    first commit

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,21 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0].toUpperCase();
+        int times = Integer.parseInt(args[1]);
+        int nameLength = word.length();
+        String anLetters = "AEFHILMNORSX";
+
+        for (int i = 0; i < nameLength; i++) {
+                char letter = word.charAt(i);
+                System.out.println("Give me "+ (anLetters.indexOf(letter)>0 ? "an " : "a ") +letter+": "+ letter+"!");
+        }
+
+        System.out.println("What does that spell?");
+
+        for (int i = 0; i < times; i++) {
+                System.out.println(word+"!!!");
+        }
+
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,32 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+    public static void main(String args[]) {
+        int seed = Integer.parseInt(args[0]);
+        String mode = args[1];
+
+        for (int i = 1; i <= seed; i++) {
+
+            int num = i;
+            int times = 1;
+            String conjecture = num + " ";
+
+            do {
+                if (num % 2 == 0) {
+                    num = num / 2;
+                } else {
+                    num = num * 3 + 1;
+                }
+                times++;
+                conjecture = conjecture + num + " ";
+            } while (num != 1);
+
+            conjecture = conjecture + "(" + times + ")";
+            if (mode.equals("v")) {
+                System.out.println(conjecture);
+            }
+        }
+
+        System.out.println("Every one of the first "+seed+" hailstone sequences reached 1.");
+
+    }
 }
